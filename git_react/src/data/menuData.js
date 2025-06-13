d773694bc3db7fcff575ebc3603909f07374334f
BeamDubai <beem094409@gmail.com>
2025-06-13 11:31:20 +0700

    menuItem

---
+++ git_react/src/data/menuData.js
@@ -0,0 +1,19 @@
+
+export const menuItems =[
+    {
+    id:1,
+    name:"ข้าวหมูทอด" ,
+    price: 50,
+    image:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pholfoodmafia.com%2Frecipe%2Fgarlic-and-pepper-pork-with-rice-%25E0%25B8%25AB%25E0%25B8%25A1%25E0%25B8%25B9%25E0%25B8%2597%25E0%25B8%25AD%25E0%25B8%2594%25E0%25B8%2581%25E0%25B8%25A3%25E0%25B8%25B0%25E0%25B9%2580%25E0%25B8%2597%25E0%25B8%25B5%25E0%25B8%25A2%25E0%25B8%25A1%25E0%25B8%259E%25E0%25B8%25A3%25E0%25B8%25B4%25E0%25B8%2581%2F&psig=AOvVaw1JbSHH4r8VDIJD2Vu4KvIK&ust=1749874860296000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCLlP3F7Y0DFQAAAAAdAAAAABAL",
+    desc:"ข้าวหมูทอด บ้านแม่ง แสนหอมฉุย"
+    },
+
+    {
+    id:2,
+    name:"ข้าวผัดกุ้ง" ,
+    price: 59,
+    image:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pholfoodmafia.com%2Frecipe%2Fgarlic-and-pepper-pork-with-rice-%25E0%25B8%25AB%25E0%25B8%25A1%25E0%25B8%25B9%25E0%25B8%2597%25E0%25B8%25AD%25E0%25B8%2594%25E0%25B8%2581%25E0%25B8%25A3%25E0%25B8%25B0%25E0%25B9%2580%25E0%25B8%2597%25E0%25B8%25B5%25E0%25B8%25A2%25E0%25B8%25A1%25E0%25B8%259E%25E0%25B8%25A3%25E0%25B8%25B4%25E0%25B8%2581%2F&psig=AOvVaw1JbSHH4r8VDIJD2Vu4KvIK&ust=1749874860296000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCLlP3F7Y0DFQAAAAAdAAAAABAL",
+    desc:"ข้าวผัดกุ้ง บ้านแม่ง แสนหอมฉุย"
+    }
+
+]
